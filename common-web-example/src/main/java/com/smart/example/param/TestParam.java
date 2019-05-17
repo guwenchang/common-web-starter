ea922424cee2fdb354f184226d112975dcf9e0d1
guwenchang <1379864599@qq.com>
2019-05-17 16:56:54 +0800

    优化开发体验

--- common-web-example/src/main/java/com/smart/example/param/TestParam.java
+++ common-web-example/src/main/java/com/smart/example/param/TestParam.java
@@ -4,12 +4,7 @@ import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
-import java.time.LocalDateTime;
 
-/**
- * @author Levin
- * @since 2018/12/17 0017
- */
 @Data
 @ApiModel
 public class TestParam {
